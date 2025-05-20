74a3268978abe12adec9acf15dfbd407a5cb8aab
tfgleison <152760227+tfgleison@users.noreply.github.com>
2025-05-20 18:10:08 +0000

    exercicio_variaveis

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,12 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(idade_numerica))
+print(type(data_nascimento))
+print(type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print("Paloma"*2)
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(46*1,75)
