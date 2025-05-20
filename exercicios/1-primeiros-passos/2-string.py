f9cd5b1f71f523ae4faa398ad91eecb9749b6381
tfgleison <152760227+tfgleison@users.noreply.github.com>
2025-05-20 17:25:19 +0000

    exercicio_string

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,16 +1,17 @@
-resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
-
 # Imprima na tela a variável "resumo"
-
+resumo = "Paloma é uma mulher de 46 anos que deseja aprender 'python."
+print(resumo)
 
 # Imprima na tela apenas a segunda letra da variável
-
+resumo = "Paloma é uma mulher de 46 anos que deseja aprender 'python."
+print(resumo[1])
 
 # Imprima na tela a idade de Paloma (resposta esperada: "46")
-
+print(resumo[23:25])
 
 # Imprima na tela o trecho final da variável
-
+resumo = "Paloma é uma mulher de 46 anos que deseja aprender 'python."
+print(resumo[:6])
 
 # Converta todos as letras para minúsculo e imprima na tela
 
