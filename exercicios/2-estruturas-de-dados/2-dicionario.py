80f382733cdb94a9f2cd0f6d9aebeff5237d34e3
tfgleison <152760227+tfgleison@users.noreply.github.com>
2025-05-21 17:01:05 +0000

    exercicio_dicionario

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -7,12 +7,20 @@ pessoa = {'nome':'Crislaine',
           'animal_estimacao':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print(pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
+valores = list(pessoa.values())
+print(valores)
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
+chaves = list(pessoa.keys())
+print(chaves)
 
 # Insira um novo par chave-valor no dicionário
+pessoa['ano_nascimento'] = 1976
+print(pessoa)
+
+# Remover um par chave-valor no dicionário
+pessoa.pop('idade')
+print(pessoa)
